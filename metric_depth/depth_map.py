c9defc570acec3d1e31bc253c9a54e24d1c3ae24
Shakya Fernando <shakya.fernando@griffithuni.edu.au>
2024-06-28 13:42:34 +1000

    :art::memo: Improved code and added Documentation

--- metric_depth/depth_map.py
+++ metric_depth/depth_map.py
@@ -1,3 +1,24 @@
+"""
+This script processes images using a depth estimation model to generate depth maps and update the input CSV file with relevant metadata (z-values).
+
+It reads the focal length and other necessary information from a CSV file, applies the model to each image, and saves
+the depth maps and corresponding metadata (z-min and z-max values) to the specified output directory and updates the input CSV file.
+
+Set global variables: 
+    MODEL: Options: "indoor" or "outdoor" - This is change which model will be used to generate depth maps
+    CAMERA: Options: "left" or "right" - This will adjust which camera focal length will be used impacting z-values
+
+Usage:
+    python script.py -m model_name -p pretrained_resource -f focal_length_file -i input_directory -o output_directory
+
+Arguments:
+    -m, --model: Name of the model to test (default: zoedepth)
+    -p, --pretrained_resource: Pretrained resource to use for fetching weights (default: local::./checkpoints/depth_anything_metric_depth_{MODEL}.pt)
+    -f, --focal_length_file: CSV file containing the focal length (required)
+    -i, --input_dir: Directory containing input images (default: ./my_test/input)
+    -o, --output_dir: Directory to save output images (default: ./my_test/output)
+"""
+
 import argparse
 import os
 import glob
@@ -19,9 +40,11 @@ NYU_DATA = True  # This uses FL as FY and FX
 FINAL_HEIGHT = 1080
 FINAL_WIDTH = 1920
 DATASET = "nyu"  # Let's not pick a fight with the model's dataloader
+MODEL = "outdoor"  # Change model here | Options: "indoor" or "outdoor"
+CAMERA = "left"  # Change camera here | Options: "left" or "right" | This will adjust which camera focal length will be used
 
 
-def process_images(model, focal_length_df, output_csv_file):
+def process_images(model, focal_length_df, focal_length_file):
     """
     Processes images using the specified depth estimation model. It reads the focal length from the CSV file,
     applies the model to each image, and saves the depth maps images to the output directory.
@@ -30,7 +53,7 @@ def process_images(model, focal_length_df, output_csv_file):
     Args:
         model: The depth estimation model.
         focal_length_df (pd.DataFrame): DataFrame containing focal length data.
-        output_csv_file (str): Path to the output CSV file.
+        focal_length_file (str): Path to the input CSV file containing the focal length.
     """
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
@@ -39,7 +62,9 @@ def process_images(model, focal_length_df, output_csv_file):
         os.path.join(INPUT_DIR, "*.jpg")
     )
 
-    results = []
+    # Ensure the new columns exist in the DataFrame
+    focal_length_df[f"z-min-{MODEL}-{CAMERA}"] = np.nan
+    focal_length_df[f"z-max-{MODEL}-{CAMERA}"] = np.nan
 
     for image_path in tqdm(image_paths, desc="Processing Images"):
         try:
@@ -62,14 +87,14 @@ def process_images(model, focal_length_df, output_csv_file):
                 print(f"No focal length found for {image_name}, skipping.")
                 continue
 
-            FL = focal_length_row["focal-length-left"].values[0]
+            FL = focal_length_row[f"focal-length-{CAMERA}"].values[0]
             deployment_code = focal_length_row["deployment-code"].values[0]
-            calibration_id = focal_length_row["calibration-id"].values[0]
+            calibration_id = focal_length_row["calibration-id"].values[0]  # exp
             frame = focal_length_row["frame"].values[0]
 
             # Print the focal length used for the current image
             print(
-                f"Processing {image_name} with FL: {FL} | deployment: {deployment_code} | cal_id: {calibration_id} | frame: {frame}"
+                f"Processing {image_name} | FL: {FL} | deployment: {deployment_code} | cal_id: {calibration_id} | frame: {frame}"
             )
 
             color_image = Image.open(image_path).convert("RGB")
@@ -105,20 +130,20 @@ def process_images(model, focal_length_df, output_csv_file):
                     (np.multiply(x, z), np.multiply(y, z), z), axis=-1
                 ).reshape(-1, 3)
 
-                z_min = z.min()
-                z_max = z.max()
+                z_min = z.min()  # These values will change depending on the MODEL used
+                z_max = z.max()  # These values will change depending on the MODEL used
 
-                results.append(
-                    {
-                        "calibration-id": calibration_id,
-                        "deployment-code": deployment_code,
-                        "frame": frame,
-                        "focal-length-left": FL,
-                        "z-min-indoor": z_min,
-                        "z-max-indoor": z_max,
-                    }
-                )
-                print(z_min, z_max)  # Print z-min and z-max
+                # Update the DataFrame with z-min and z-max values
+                focal_length_df.loc[
+                    (focal_length_df["calibration-id"] == calibration_id)
+                    & (focal_length_df["deployment-code"] == int(deployment_code))
+                    & (focal_length_df["frame"] == int(frame)),
+                    [f"z-min-{MODEL}-{CAMERA}", f"z-max-{MODEL}-{CAMERA}"],
+                ] = [z_min, z_max]
+
+                print(
+                    f"z-min-{MODEL}: {z_min} | z-max-{MODEL}: {z_max}"
+                )  # Print z-min and z-max
 
                 z_norm = (z - z_min) / (z_max - z_min)
                 imgdepth = o3d.geometry.Image((z_norm * 255).astype(np.uint8))
@@ -133,9 +158,8 @@ def process_images(model, focal_length_df, output_csv_file):
         except Exception as e:
             print(f"Error processing {image_path}: {e}")
 
-    # Create a new DataFrame with the results and save it to a new CSV file
-    results_df = pd.DataFrame(results)
-    results_df.to_csv(output_csv_file, index=False)
+    # Save the updated DataFrame back to the input CSV file
+    focal_length_df.to_csv(focal_length_file, index=False)
 
 
 def main(
@@ -144,7 +168,6 @@ def main(
     focal_length_file,
     input_dir,
     output_dir,
-    output_csv_file,
 ):
     """
     Main function that sets up the model and processes images using the specified parameters.
@@ -155,7 +178,6 @@ def main(
         focal_length_file (str): CSV file containing the focal length.
         input_dir (str): Directory containing input images.
         output_dir (str): Directory to save output images.
-        output_csv_file (str): Path to the output CSV file.
     """
     global INPUT_DIR, OUTPUT_DIR
 
@@ -166,10 +188,10 @@ def main(
     OUTPUT_DIR = output_dir  # Images output directory
 
     config = get_config(model_name, "eval", DATASET)
-    config.pretrained_resource = pretrained_resource
+    config.pretrained_resource = pretrained_resource.format(MODEL=MODEL)
     model = build_model(config).to("cuda" if torch.cuda.is_available() else "cpu")
     model.eval()
-    process_images(model, focal_length_df, output_csv_file)
+    process_images(model, focal_length_df, focal_length_file)
 
 
 if __name__ == "__main__":
@@ -181,7 +203,7 @@ if __name__ == "__main__":
         "-p",
         "--pretrained_resource",
         type=str,
-        default="local::./checkpoints/depth_anything_metric_depth_indoor.pt",
+        default="local::./checkpoints/depth_anything_metric_depth_{MODEL}.pt",  # Default model
         help="Pretrained resource to use for fetching weights.",
     )
     parser.add_argument(
@@ -205,13 +227,6 @@ if __name__ == "__main__":
         default="./my_test/output",
         help="Directory to save output images.",
     )
-    parser.add_argument(
-        "-oc",
-        "--output_csv_file",
-        type=str,
-        required=True,
-        help="Path to the output CSV file.",
-    )
 
     args = parser.parse_args()
     main(
@@ -220,5 +235,4 @@ if __name__ == "__main__":
         args.focal_length_file,
         args.input_dir,
         args.output_dir,
-        args.output_csv_file,
     )
